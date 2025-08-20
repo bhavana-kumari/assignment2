a4217fd4d94ca4333ce1a472cfce8bf258878e2d
bhavana <bhavanakhadiya8054@gmail.com>
2025-08-20 17:19:09 +0530

    added java assignment 2

---
+++ kmtom.java
@@ -0,0 +1,16 @@
+public class kmtom {
+    
+    public static void main(String[] args) {
+        double radiusKm = 6378;
+        double pi = Math.PI;
+
+        double volumeKm = (4.0 / 3.0) * pi * Math.pow(radiusKm, 3);
+        double kmToMiles = 1.6;
+        double radiusMiles = radiusKm / kmToMiles;
+        double volumeMiles = (4.0 / 3.0) * pi * Math.pow(radiusMiles, 3);
+
+        System.out.println("The volume of earth in cubic kilometers is " + volumeKm + " and cubic miles is " + volumeMiles);
+    }
+}
+
+    
---
+++ udis.java
@@ -0,0 +1,23 @@
+
+    import java.util.Scanner;
+    public class udis {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the student fee: ");
+        double fee = input.nextDouble();
+
+        System.out.print("Enter the university discount percent: ");
+        double discountPercent = input.nextDouble();
+
+        double discount = (discountPercent / 100) * fee;
+        double finalFee = fee - discount;
+
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+    }
+}
+
+    
+
